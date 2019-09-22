dfcab922cc7dbd82bf799bbd9eeb01b326b82e70
namoshika <namoshika@users.noreply.github.com>
2019-09-23 08:31:17 +0900

    pipenv廃止
    パッケージインストールで余計なエラーが生じる他、ロックが遅い

---
+++ .build/start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+jupyter notebook password && jupyter lab --ip 0.0.0.0 --allow-root
--- 02_launch.sh
+++ 02_launch.sh
@@ -2,5 +2,4 @@
 
 docker run \
     --name wkspace-ml -it --rm --mount type=bind,src=$PWD,dst=/workspace \
-    -p 8888:8888 --runtime nvidia learning-ml \
-    /bin/bash --login -ic 'pipenv run jupyter notebook password && pipenv run jupyter lab --ip 0.0.0.0 --allow-root'
+    -p 8888:8888 --runtime nvidia learning-ml
